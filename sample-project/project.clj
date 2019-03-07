7683bf5a56099030ff08147cc3e27e0aafbc228d
conao3 <conao3@gmail.com>
2019-03-08 03:43:14 +0900

    apply AGPLv3

--- sample-project/project.clj
+++ sample-project/project.clj
@@ -1,7 +1,7 @@
-(defproject sample-project "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject sample-project "0.1.0"
+  :description "lambda-clojure-helper sample project"
+  :url "http://github.com/conao3/lambda-clojure-helper"
+  :license {:name "Affero General Public License version 3 or lator"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :repl-options {:init-ns sample-project.core})
