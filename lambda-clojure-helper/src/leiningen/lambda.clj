913e412e31775a8d72ca54650c2c844994dbe787
conao3 <conao3@gmail.com>
2019-03-14 03:22:08 +0900

    rename command name

--- lambda-clojure-helper/src/leiningen/lambda.clj
+++ lambda-clojure-helper/src/leiningen/lambda.clj
@@ -1,10 +1,16 @@
-(ns leiningen.aws
+(ns leiningen.lambda
   (:require [leinjacker.deps :as deps]
             [leinjacker.eval :as eval]
             [lambda-clojure-helper.core :as helper]))
 
-(defn aws
+(defn lambda
   "Deploy Clojure functions to AWS lambda."
   [project & args]
   ;; (leiningen.core.main/info "Hello AWS!")
   (apply helper/-main args))
+
+
+
+
+
+
