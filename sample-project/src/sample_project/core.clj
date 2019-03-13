450c33c63972971ef810a8824b7144c358906b06
conao3 <conao3@gmail.com>
2019-03-13 23:40:08 +0900

    define hello-lambda function

--- sample-project/src/sample_project/core.clj
+++ sample-project/src/sample_project/core.clj
@@ -1,5 +1,11 @@
 (ns sample-project.core)
 
+(defn hello-lambda [event]
+  {'statusCode 200
+   'headers {'content-type "text/plain"}
+   'body "hello world"
+   })
+
 (defn foo
   "I don't do a whole lot."
   [x]
